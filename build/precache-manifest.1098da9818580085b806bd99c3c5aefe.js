493fe718e44053a9248444c5274e8da0894f605f
Sini Suihkonen <sahsuihkonen@gmail.com>
2020-02-09 00:48:06 +0200

    frontend

---
+++ build/precache-manifest.1098da9818580085b806bd99c3c5aefe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73c0b46645fbfa2c58fcd0a0a0a1d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d695c7690f715abdaca",
+    "url": "/static/css/main.8f6e3e72.chunk.css"
+  },
+  {
+    "revision": "a4ff7f15b0a9c86bc646",
+    "url": "/static/js/2.b60c7687.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b60c7687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d695c7690f715abdaca",
+    "url": "/static/js/main.ed9a18d9.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
